566510d3bef01ca88f95157b0538e7a66626696c
Julia <loghinova.86@gmail.com>
2023-10-15 23:09:30 +0300

    addselectors

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,29 +1,33 @@
+import { createSelector } from "reselect";
 
 
-export const getUsers = () => {
+const getUsersSelector = () => {
     return state.usersPage.users;
-}
+};
+
+export const getUsers = createSelector( getUsersSelector,  (users) => {
+    users.filter( u => true);
+});
 
 export const getPageSixe = () => {
     return state.usersPage.pageSize;
-}
+};
 
 export const getTotalUsersCount = () => {
     return state.usersPage.totalUsersCount;
-}
+};
 
 export const getCurrentPage = () => {
     return state.usersPage.currentPage;
-}
+};
 
 export const getIsFetching = () => {
     return state.usersPage.isFetching;
-}
+};
 
 export const getFollowingInProgress = () => {
     return state.usersPage.followingInProgress;
-}
-
+};
 
 
 
