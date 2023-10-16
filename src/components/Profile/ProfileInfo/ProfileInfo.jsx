4a0b78b11744a240cd9211ab21afd352ecca9f79
Julia <loghinova.86@gmail.com>
2023-10-16 22:35:55 +0300

    hook

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
-import PreLoader from "../../commen/Preloader/Preloader";
-import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -15,7 +14,7 @@ const ProfileInfo = (props) => {
       </div>
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} />
-        <ProfileStatus status = {props.status} updateStatus = {props.updateStatus} />
+        <ProfileStatusWithHooks status = {props.status} updateStatus = {props.updateStatus} />
       </div>
     </div>
   );
