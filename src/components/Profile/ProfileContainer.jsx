69c58e535e6c8729d1c7a4b2968eea5abf331b31
Julia <loghinova.86@gmail.com>
2023-10-29 18:07:27 +0300

    profile update

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -47,14 +47,16 @@ class ProfileContainer extends React.Component {
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile,
-    status: state.profilePage.status,
-    authorizedUserId: state.auth.userId,
-    isAuth: state.auth.isAuth,
+    return ({
+        profile: state.profilePage.profile,
+        status: state.profilePage.status,
+        authorizedUserId: state.auth.userId,
+        isAuth: state.auth.isAuth,
+        })  
 });
 
 export default compose (
-    connect (mapStateToProps, {getUserProfile, getStatus, updateStatus, savePhoto}),
+    connect (mapStateToProps, {getUserProfile, getStatus, updateStatus, savePhoto, saveProfile}),
     withRouter
 ) (ProfileContainer);
 
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./ProfileInfo.module.css";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 import userPhoto from '../../../assets/images/images.png';
+import ProfileDataForm from "./ProfileDataForm";
 
-const ProfileInfo = (profile, status, updateStatus, isOwner) => {
-  if (!profile) {
+const ProfileInfo = (profile, status, updateStatus, isOwner, saveProfile) => {
+  
+    let [editMode, setEditMode] = useState(false);
+  
+    if (!profile) {
     return <Preloader />;
   }
 
@@ -14,6 +18,10 @@ const ProfileInfo = (profile, status, updateStatus, isOwner) => {
     }
   }
 
+  const onSubmit = (formData) => {
+    console.log(formData);
+  }
+
   return (
     <div>
       <div>
@@ -21,11 +29,45 @@ const ProfileInfo = (profile, status, updateStatus, isOwner) => {
       </div>
       <div className={s.descriptionBlock}>
         <img src={profile.photos.large || userPhoto} className={s.mainPhoto}/>
-        { isOwner && <input type={'file'} onChange={} /> }
+        { isOwner && <input type={'file'} onChange={onMainPhotoSelected} /> }
+        { editMode ? <ProfileDataForm profile = {profile} onSubmit={onSubmit} /> : <ProfileData goToEditMode={() => {setEditMode(true)}} profile = {profile} isOwner = {isOwner} /> }
+        <ProfileData profile={profile} />
         <ProfileStatusWithHooks status = {status} updateStatus = {updateStatus} />
       </div>
     </div>
   );
 };
+
+const ProfileData = ({profile, isOwner, goToEditMode}) => {
+    return <div>
+            {isOwner && <div>
+                <button onClick={goToEditMode}>edit</button>
+            </div>}
+            <div>
+                <b>Full name</b>: {profile.fullName ? 'yes' : 'no'}
+            </div>
+            <div>
+                <b>Looking for a job</b>: {profile.lookingForAJob ? 'yes' : 'no'}
+            </div>
+            {profile.lookingForAJob &&
+            <div>
+                <b>My professional skills</b>: {profile.lookingForAJobDescription}
+            </div>
+            }
+            <div>
+                <b>About me</b>: {profile.aboutMe}
+            </div>
+            <div>
+                <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+                    return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+                })}
+            </div>
+        </div>
+}
+
+
+const Contact = ({contactTitle, contactValue}) => {
+    return <div className={s.contact}><b>{contactTitle}</b>: {contactValue}</div>
+}
 
 export default ProfileInfo;
